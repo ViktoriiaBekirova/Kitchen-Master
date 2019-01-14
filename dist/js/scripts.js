a465ab4230b93b463ce3246fa35b010f87f144f0
bekirova <viktoriia.bekirova@gmail.com>
2019-01-14 15:42:05 +0300

    edits

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,10 +1,16 @@
 $(function () {
-    $('.menu_toggle').on('click', function () {
+    $('.menu-toggle').on('click', function () {
         $(this).toggleClass('rotate');
-        $('.row_menu').slideToggle(300, function () {
+        $('.row__menu').slideToggle(300, function () {
             if ($(this).css('display') === 'none') {
                 $(this).removeAttr('style'); // удалит атрибут
             }
         });
     });
+    if ($(window).width() < 720) {
+        $('.nav-link').on('click', function () {
+            $('.menu-toggle').removeClass('rotate')
+            $('.row__menu').slideUp(300)
+        })
+    }
 });
